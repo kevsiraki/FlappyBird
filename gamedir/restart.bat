12d8c6e8fa60a53564e7372450af403307d6c4b6
Kevin Siraki <kevsiraki@gmail.com>
2016-07-25 12:09:34 -0700

    Update restart.bat

--- gamedir/restart.bat
+++ gamedir/restart.bat
@@ -1,6 +1,10 @@
 @echo off
 color a
-mode con: cols=160 lines=1
+mode con: cols=20 lines=1
 set /p answer=Restart (y/n)?:
 if %answer%==y start "" "C:\Users\kevsi\Desktop\gamedir\killre.bat"
-pause
+if %answer%==n taskkill /IM pythonw.exe /F
+set /p answer3=Enter Score (y/n)?:
+if %answer3%==y start "" "C:\Users\kevsi\Desktop\gamedir\main\Last Score.py"
+if %answe3r%==n taskkill /IM pythonw.exe /F
+
